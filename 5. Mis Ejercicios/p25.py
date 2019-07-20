058949e0e4b1414442d4e5ce243e6d7fcecccd0e
javierramon23 <red-dragon-23@Javiers-MacBook-Air.local>
2019-07-20 14:21:56 +0200

    Actualizacion ejemplos ejercicios ficheros

--- 5. Mis Ejercicios/p25.py
+++ 5. Mis Ejercicios/p25.py
@@ -154,11 +154,145 @@ csv_usuario = '{},{},{},{}'.format(usuario['nombre'], usuario['apellido1'], usua
 
 # INVOCAMOS A LA FUNCION PASANDOLE LA CADENA GENERADA CON LOS DATOS DEL DICCIONARIO.
 print('\n1.- Se almacena la información del DICCIONARIO en el fichero "salida.txt".')
+
 guardar(csv_usuario)
+
 print('2.- La información ha sido almacenada.')
+
 print('3.- Se lee la información del fichero "salida.txt".')
-# INVOCAMOS ALA FUNCION DE LECTURA Y ALMACENAMOS SU RETORNO EN UNA VARIABLE
+
+# INVOCAMOS A LA FUNCION DE LECTURA Y ALMACENAMOS SU RETORNO EN UNA VARIABLE
 registro = leer()
+
 print('4.- Se muestra la información leida desde el fichero.\n')
+
 # IMPRIMIMOS EL REGISTRO LEIDO DEL FICHERO
 print(registro)
+print('')
+
+
+'''
+    OTRO EJEMPLO
+'''
+print('EJEMPLO de ESCRITURA Y LECTURA DE UN FICHERO COMPLETO:')
+
+# Crea REGISTROS de VENTAS en FORMATO TEXTO SEPARADO POR ';'.
+def crearRegistro(codigo, descripcion, precioUnidad, numeroItems = 1):
+    # El METODO 'format()' es propio de los STRINGS, NO DEL METODO 'print()' por lo que puede utilizarse DIRECTAMENTE.
+    return '{};{};{};{}\n'.format(codigo, descripcion, precioUnidad, numeroItems * precioUnidad)
+
+
+# FORMA MAS SEGURA de ABRIR un FICHERO, NO NOS PREOCUPAMOS de CERRARLO.
+with open('ventas.txt', 'w+') as fDestino:
+    fDestino.write(crearRegistro(1, 'Producto 1', 10.25))
+    fDestino.write(crearRegistro(2, 'Producto 2', 20.50, 2))
+    fDestino.write(crearRegistro(3, 'Producto 3', 30.75, 3))
+    fDestino.write(crearRegistro(4, 'Producto 4', 40, 4))
+
+# FORMA MAS SEGURA de ABRIR un FICHERO, NO NOS PREOCUPAMOS de CERRARLO.
+with open('ventas.txt', 'r') as fOrigen:
+    ventas = fOrigen.read()
+    # Podemos especificar el NUMERO de CARACTERES que se van a leer.
+    # ventas = fOrigen.read(10)
+
+# FORMA TRADICIONAL DE APERTURA DE UN FICHERO
+
+# fDestino = open('ventas.txt', 'w+')
+
+# Se CREAN unos REGISTROS y se GUARDAN en el FICHERO.
+# fDestino.write(crearRegistro(1, 'Producto 1', 2))
+# fDestino.write(crearRegistro(2, 'Producto 2', 3, 2))
+# fDestino.write(crearRegistro(3, 'Producto 3', 2.5, 4))
+# fDestino.write(crearRegistro(4, 'Producto 4', 1.75))
+
+# Es OBLIGATORIO CERRAR el FICHERO ANTES de FINALIZAR.
+# fDestino.close()
+
+
+
+# FORMA TRADICIONAL DE APERTURA DE UN FICHERO
+
+# fOrigen = open('ventas.txt', 'r')
+# ventas = fOrigen.read()
+# fOrigen.close()
+
+# Se IMPRIME el CONTENIDO del FICHERO:
+print(ventas)
+
+'''
+    OTRO EJEMPLO MAS
+'''
+print('EJEMPLO de LECTURA de un FICHERO ENTERO X LINEAS:\n')
+
+# Se LEE el FICHERO de FORMA 'OPTIMA'.
+with open('ventas.txt', 'r') as fOrigen:
+    # readlines() CREA una LISTA con TODAS las LINEAS del FICHERO.
+    lineasFichero = fOrigen.readlines()
+    # Podemos especificar el NUMERO de CARACTERES que se van a leer.
+    # En el caso de readlines() si el numero de caracteres es inferior al de una linea se lee la LINEA ENTERA.
+    # lineasFichero = fOrigen.readlines(24)
+
+# Recorremos la LISTA e imprimimos cada ELEMENTO.
+for linea in lineasFichero:
+    print(linea)
+
+'''
+    OTRO EJEMPLO MAS
+'''
+print('EJEMPLO de LECTURA de UNA LINEA de un FICHERO:\n')
+
+with open('ventas.txt', 'r') as fOrigen:
+    lineaFichero = fOrigen.readline()
+    # Podemos especificar el NUMERO de CARACTERES que se van a leer.
+    # Si se SOBREPASA el MAXIMO de CARACTERES devuelve la LINEA COMPLETA.
+    lineaFichero = fOrigen.readline(10)
+
+print(lineaFichero)
+
+'''
+    OTRO EJEMPLO MAS
+'''
+print('EJEMPLO LECTURA SECUENCIAL DE UN FICHERO: Vamos a LEER EL FICHERO COMPLETO pero LINEA x LINEA:')
+print('\nEN FORMATO TEXTO\n')
+
+def formatearRegistro(linea):
+    # Eliminamos el SALTO DE LINEA del FINAL de la LINEA.
+    linea = linea[:-1]
+    # Se crea una LISTA con lada uno de los CAMPOS de la LINEA.
+    campos = linea.split(';')
+    # Se crea y se DEVUELVE un DICCIONARIO para GUARDAR la INFO de CADA LINEA.
+    return {
+            'codigo':campos[0],
+            'descripcion': campos[1],
+            'unidad': campos[2],
+            'total': campos[3],
+        }
+
+with open('ventas.txt', 'r') as fOrigen:
+    lineaFichero = fOrigen.readline()
+    # Cuando no quedan mas LINEAS en el FICHERO, readline() DEVUELVE la CADENA VACIA.
+    while lineaFichero != '':
+        # Con cada LINEA del fichero obtenemos un DICCIONARIO con la INFO.
+        registro = formatearRegistro(lineaFichero)
+        # Formateamos la SALIDA.
+        print('Cod. Prod: {}\tDescripción: {}\tPrecio Unidad: {}€\tTotal: {}€'.format(registro['codigo'],
+                                                                                           registro['descripcion'],
+                                                                                           registro['unidad'],
+                                                                                           registro['total']))
+        # Se LEE otra LINEA del FICHERO.
+        lineaFichero = fOrigen.readline()
+
+'''
+    OTRO EJEMPLO MAS
+'''
+print('EJEMPLO LECTURA SECUENCIAL DE UN FICHERO: Vamos a LEER EL FICHERO COMPLETO pero LINEA x LINEA:')
+print('\nEN FORMATO BINARIO\n')
+
+
+
+
+
+
+
+
+
